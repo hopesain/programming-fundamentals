32bc7c27765d4ea08a913035dc9eaf1390879f8c
hopesain <hopessain@gmail.com>
2025-09-23 13:27:52 +0100

    open closed principle completed.

--- 05_solid_principles/02_ocp.py
+++ 05_solid_principles/02_ocp.py
@@ -1,37 +1,79 @@
+from abc import ABC, abstractmethod
 
-SAVED_PASSWORD = "1234"
-SAVED_PATTERN = "L"
-SAVED_FINGERPRINT = "F"
+STORED_PASSWORD = "1234"
+STORED_PATTERN = "L"
+STORED_FINGERPRINT = "F"
+
 
 class BadScreenLock:
     def __init__(self):
-        self.__is_device_locked = True
+        self.is_locked = True
 
-    def unlock_screen(self, unlock_method, password=None, pattern=None, fingerprint=None, face_recognition=None):
-        if unlock_method == "Password":
-            if password == SAVED_PASSWORD:
-                self.__is_device_locked = False
-                return f"Your device is now unlocked."
-            self.__is_device_locked = True
-            return f"Incorrect password."
-        elif unlock_method == "Pattern":
-            if pattern == SAVED_PATTERN:
-                self.__is_device_locked = False
-                return f" Your device is now unlocked."
-            return f"Incorrect pattern."
+    def unlock_screen(self, unlock_method, password=None, pattern=None, fingerprint=None):
+        if unlock_method == "password":
+            if password == STORED_PASSWORD:
+                self.is_locked = False
+                return "Screen Unlocked"
+            self.is_locked = True
+            return "Incorrect Password"
+        elif unlock_method == "pattern":
+            if pattern == STORED_PATTERN:
+                self.is_locked = False
+                return "Screen Unlocked"
+            self.is_locked = True
+            return "Incorrect Pattern"
         elif unlock_method == "fingerprint":
-            if pattern == SAVED_FINGERPRINT:
-                self.__is_device_locked = False
-                return f" Your device is now unlocked."
-            return f"Incorrect fingerprint."
+            if fingerprint == STORED_FINGERPRINT:
+                self.is_locked = False
+                return "Screen Unlocked"
+            self.is_locked = True
+            return "Incorrect Fingerprint"
         
+
+class UnlockStrategy(ABC):
+    @abstractmethod
+    def unlock(self):
+        pass
+
+class PasswordStrategy(UnlockStrategy):
+    def unlock(self, credential):
+        if not credential == STORED_PASSWORD:
+            raise Exception("Incorrect Password")
+        return True
+
+class PatternStrategy(UnlockStrategy):
+    def unlock(self, credential):
+        if not credential == STORED_PATTERN:
+            raise Exception("Incorrect Pattern")
+        return True
     
-    def lock_screen(self):
-        self.__is_device_locked = True
-        return f"Your device is locked."
-    
+class FingerprintStrategy(UnlockStrategy):
+    def unlock(self, credential):
+        if not credential == STORED_FINGERPRINT:
+            raise Exception("Incorrect Fingerprint")
+        return True
 
-first_unlock = ScreenLock()
-print(first_unlock.unlock_screen("Password", password="1237"))
+class ScreenLock:
+    def __init__(self):
+        self.is_locked = True
+
+    def unlock_screen(self, strategy: UnlockStrategy, credential):
+        if strategy.unlock(credential):
+            self.is_locked = False
+            return "Screen Unlocked"
+        self.is_locked = True
+        return "Unlock Failed"
 
 
+if __name__ == "__main__":
+    bad_lock = BadScreenLock()
+    print(bad_lock.unlock_screen("password", password="1234"))
+    print(bad_lock.unlock_screen("pattern", pattern="X")) 
+    print(bad_lock.unlock_screen("fingerprint", fingerprint="F"))
+
+    print("------------------------------------")
+
+    good_lock = ScreenLock()
+    print(good_lock.unlock_screen(PasswordStrategy(), "1234"))  
+    print(good_lock.unlock_screen(PatternStrategy(), "S"))  
+    print(good_lock.unlock_screen(FingerprintStrategy(), "F"))  
